3a30b17c73ce4bea0d36794620f95202ae0ea758
JolantaWro <jolanta.woronowska@gmail.com>
2023-03-30 22:19:04 +0200

    task 1, withTime

--- 03_Dzien_4/01_Komponenty/01_Zadanie_1/js/components/Stopper.js
+++ 03_Dzien_4/01_Komponenty/01_Zadanie_1/js/components/Stopper.js
@@ -1,9 +1,17 @@
 import React from "react";
+import withTimer from "../hoc/withTimer"
 
 // Zamień ciało funkcji tak aby renderowała Stopper wg. treści zadania
-const Stopper = () => <div />;
+const Stopper = (props) =>
+    <>
+        <button onClick={props.startTime}>Start</button>
+        <button onClick={props.stopTime}>Stop</button>
+        <p>Counter: {props.counter}</p>
+        {props.children}
+
+    </>
 
 // Zamień deklarację komponentu poniżej tak aby korzystał z wczesniej zamiplementowanego HOC withTimer
-const StopperWithTimer = () => <div />;
+const StopperWithTimer = withTimer(Stopper)
 
 export { Stopper, StopperWithTimer };
