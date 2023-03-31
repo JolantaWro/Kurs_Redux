8ff5e146744d84d0f8b9dd25b17dda7f81e19679
JolantaWro <jolanta.woronowska@gmail.com>
2023-03-31 23:24:19 +0200

    add list in ShoppingList

--- 04_Dzien_5-7/01_Zadania_podsumowujace/01_Zadanie/js/apps/ShoppingList.js
+++ 04_Dzien_5-7/01_Zadania_podsumowujace/01_Zadanie/js/apps/ShoppingList.js
@@ -1,3 +1,6 @@
+import store from "../redux/store";
+import {ADD_PRODUCT, addProduct} from "../redux/actions";
+
 class ShoppingList {
   constructor(rootElement) {
     this.createUI(rootElement);
@@ -15,11 +18,10 @@ class ShoppingList {
           <div class="input-group mb-3">
             <input type="text" class="form-control" placeholder="Produkt..." />
             <div class="input-group-append">
-              <button class="btn btn-outline-primary" type="button" id="shop-add">Dodaj</button>
+              <button class="btn btn-outline-primary" type="submit" id="shop-add">Dodaj</button>
             </div>
           </div>
         </form>
-
         <ul class="list-group mt-3" id="shop-list">
         </ul>
       </div>
@@ -28,12 +30,35 @@ class ShoppingList {
 
   collectRefs() {
     this.form = this.rootElement.querySelector("form");
+    this.inputValue = this.rootElement.querySelector(".form-control");
     this.shopList = this.rootElement.querySelector("#shop-list");
   }
 
-  reduxConnect() {}
+  reduxConnect() {
+    store.subscribe(() => {
+      this.createLi()
+    })
+    store.subscribe(() => console.log(store.getState().products))
 
-  applyHandlers() {}
+  }
+
+  createLi() {
+    const elementLi = document.createElement("Li")
+    elementLi.classList = "list-group-item d-flex justify-content-between align-items-center"
+    store.getState().products.forEach(el => elementLi.innerText = el)
+    // elementLi.innerText = store.getState().products.forEach(el => el)
+    this.shopList.appendChild(elementLi)
+  }
+
+  applyHandlers() {
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      if(this.inputValue.value) {
+        store.dispatch(addProduct(this.inputValue.value));
+        this.inputValue.value = "";
+      }
+    })
+  }
 }
 
 export default ShoppingList;
--- 04_Dzien_5-7/01_Zadania_podsumowujace/01_Zadanie/js/redux/reducers.js
+++ 04_Dzien_5-7/01_Zadania_podsumowujace/01_Zadanie/js/redux/reducers.js
@@ -2,3 +2,34 @@
 //   counter,
 //   products zad 2
 // });
+import {ADD_PRODUCT, DECREMENT, INCREMENT} from "./actions";
+
+import {combineReducers} from "redux";
+
+const stateCounter = 0;
+const stateProduct = [];
+const counter = (state=stateCounter, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return state + 1;
+        case DECREMENT:
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+const products = (state=stateProduct, action) => {
+    switch (action.type) {
+        case ADD_PRODUCT:
+            return [...state, action.payload];
+        default:
+            return state
+
+    }
+}
+
+export default combineReducers({
+    counter,
+    products
+})
