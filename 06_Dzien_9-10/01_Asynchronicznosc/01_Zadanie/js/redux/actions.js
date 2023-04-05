c1b14b2e512e14f287f001e817643de5ab1f13ca
JolantaWro <jolanta.woronowska@gmail.com>
2023-04-05 21:14:43 +0200

    add action and reducer

--- 06_Dzien_9-10/01_Asynchronicznosc/01_Zadanie/js/redux/actions.js
+++ 06_Dzien_9-10/01_Asynchronicznosc/01_Zadanie/js/redux/actions.js
@@ -1,1 +1,20 @@
-// Plik z akcjami
+const QUOTE_FETCHING = "QUOTE_FETCHING";
+const QUOTE_FETCHED = "QUOTE_FETCHED";
+const QUOTE_ERROR = "QUOTE_ERROR";
+
+const startFetching = () => ({
+    type: QUOTE_FETCHING
+});
+
+const quoteFetched = (value) => ({
+    type: QUOTE_FETCHED,
+    payload: {value}
+
+});
+
+const quoteError = (value) => ({
+    type: QUOTE_ERROR,
+    payload: value
+});
+
+export { QUOTE_FETCHING, QUOTE_FETCHED, QUOTE_ERROR, startFetching, quoteError, quoteFetched }
--- 06_Dzien_9-10/01_Asynchronicznosc/01_Zadanie/js/redux/reducer.js
+++ 06_Dzien_9-10/01_Asynchronicznosc/01_Zadanie/js/redux/reducer.js
@@ -1,3 +1,39 @@
-// export default combineReducers({
-//   quote,
-// });
+import {QUOTE_ERROR, QUOTE_FETCHED, QUOTE_FETCHING} from "./actions";
+import {combineReducers} from "redux";
+
+
+const initialState = {
+    loading: false,
+    error: "",
+    quote: null
+}
+
+function reducerQuote (state=initialState, action) {
+    switch (action.type){
+        case QUOTE_FETCHING:
+            return {
+                ...state,
+                loading: true
+            }
+        case QUOTE_FETCHED:
+            return {
+                loading: false,
+                quote: action.payload,
+                error: null
+            }
+        case QUOTE_ERROR:
+            return {
+                loading: false,
+                quote: null,
+                error: action.payload
+            }
+        default:
+            return state
+    }
+}
+
+const rootReducers = combineReducers({
+    quote: reducerQuote
+})
+
+export default rootReducers;
